c904c001f92d4f472e4cd48c11be2291870f4383
TunaKiet <168570423+TunaKiet@users.noreply.github.com>
2025-05-16 19:18:26 +0700

    Delete chatbox.js

--- homepage/chatbox.js
+++ homepage/chatbox.js
@@ -1,12 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const toggleBtn = document.getElementById('toggle-chat-btn');
-  const chatBox = document.getElementById('chat-box');
 
-  toggleBtn.addEventListener('click', () => {
-    if (chatBox.style.display === 'none' || chatBox.style.display === '') {
-      chatBox.style.display = 'block';
-    } else {
-      chatBox.style.display = 'none';
-    }
-  });
-});
